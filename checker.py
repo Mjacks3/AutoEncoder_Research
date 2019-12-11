4a65a54014b34d59fcea68cdbf789c84fdd8f565
Malik <mjacks3@umbc.edu>
2019-12-10 20:52:02 -0500

    fix louvain + edglist files

--- checker.py
+++ checker.py
@@ -2,13 +2,16 @@ import os, sys, argparse,json, time, subprocess
 
 edgelist_cnt = 0
 embedding_cnt = 0
+
 for r, d, f in os.walk("experiment/test"):
 
-    if len(f) == 1:
-         edgelist_cnt +=1
     if len(f) == 2:
+         edgelist_cnt +=1
+    elif len(f) == 3:
+        print(f)
         edgelist_cnt +=1
         embedding_cnt += 1
+
 print(edgelist_cnt)
 print(embedding_cnt)
 
