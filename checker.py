9071cf54615e156d690bd79a580cc6eab3e75e7f
Malik <mjacks3@umbc.edu>
2019-12-12 04:06:32 +0000

    buggy condition

--- checker.py
+++ checker.py
@@ -4,13 +4,17 @@ edgelist_cnt = 0
 embedding_cnt = 0
 
 for r, d, f in os.walk("experiment/test"):
-
+    """
     if len(f) == 2:
          edgelist_cnt +=1
     elif len(f) == 3:
         edgelist_cnt +=1
         embedding_cnt += 1
-
+    """
+    if len(f) == 4:
+        edgelist_cnt += 1
+    embedding_cnt += 1
+        
 print(edgelist_cnt)
 print(embedding_cnt)
 
