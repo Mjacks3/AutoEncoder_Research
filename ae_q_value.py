be7689692fda56103d473c1357ad2fcae39ff9cc
Malik <mjacks3@umbc.edu>
2019-12-13 12:58:03 -0500

    update

--- ae_q_value.py
+++ ae_q_value.py
@@ -5,6 +5,7 @@ import  matplotlib.pyplot as plt
 source_dir = "experiment/test"
 
 #Analyis meaning number of microservices generated
+louvain = []
 clus_2 = []
 clus_3 = []
 clus_4 = []
@@ -24,10 +25,8 @@ clus_17 = []
 clus_18 = []
 clus_19 = []
 
-full_set = [clus_2,clus_3,clus_4,clus_5,clus_6,clus_7,clus_8,clus_9,clus_10,clus_11,clus_12,clus_13,clus_14,clus_15, clus_16,clus_17,clus_18,clus_19]
 
-#Note: Take you louvain out. THis is the unoptomized, just pure Qvlues
-#There needs to be another with optimized qvalues
+full_set = [louvain, clus_2,clus_3,clus_4,clus_5,clus_6,clus_7,clus_8,clus_9,clus_10,clus_11,clus_12,clus_13,clus_14,clus_15, clus_16,clus_17,clus_18,clus_19]
 
 
 
@@ -38,7 +37,7 @@ for r, d, f in os.walk(source_dir):
     #First get the edgelist to process louvain and clustering
     if r.split("/")[-1]+".embedding" in f:
         for a_file in f:
-            if  ".clustering" in a_file:
+            if  ".clustering" in a_file or "louvain" in a_file :
                 edge_list = r+'/'+r.split("/")[-1]+".txt"
 
                 with open(r+'/'+ a_file) as fi:
@@ -94,30 +93,32 @@ for r, d, f in os.walk(source_dir):
                 #print("Modularity Q Value")
                 #print(modQ)
                 #output this qvalue to file
-                
 
-                if "2.clustering" in a_file:
+
+      
+
+                if "_2.clustering" in a_file:
                     clus_2.append(modQ)
 
-                elif "3.clustering" in a_file:
+                elif "_3.clustering" in a_file:
                     clus_3.append(modQ)
 
-                elif "4.clustering" in a_file:
+                elif "_4.clustering" in a_file:
                     clus_4.append(modQ)
 
-                elif "5.clustering" in a_file:
+                elif "_5.clustering" in a_file:
                     clus_5.append(modQ)
 
-                elif "6.clustering" in a_file:
+                elif "_6.clustering" in a_file:
                     clus_6.append(modQ)
 
-                elif "7.clustering" in a_file:
+                elif "_7.clustering" in a_file:
                     clus_7.append(modQ)
 
-                elif "8.clustering" in a_file:
+                elif "_8.clustering" in a_file:
                     clus_8.append(modQ)
 
-                elif "9.clustering" in a_file:
+                elif "_9.clustering" in a_file:
                     clus_9.append(modQ)
 
                 elif "10.clustering" in a_file:
@@ -149,17 +150,32 @@ for r, d, f in os.walk(source_dir):
 
                 elif "19.clustering" in a_file:
                     clus_19.append(modQ)
+                
+                elif "louvain" in a_file:
+                    louvain.append(modQ)
+
+
+        #print(r+"/"+r.split("/")[-1] +".qvalues")
+
+        
+        with open(r+"/"+r.split("/")[-1] +".qvalues",'w+') as f:
+            for clus, num_clus in zip(full_set, range(1,20)):
+                if num_clus == 1 :
+                    f.write("louvain " + str(clus[-1])+"\n")
+                else:
+                    f.write(str(num_clus) +" " + str(clus[-1])+"\n")
+
+        
 
 
 fig = plt.figure()
-fig.suptitle('Microservice A.E. Q-Value Analysis for  750 Repository DataSet', fontsize=14, fontweight='bold')
 
 ax = fig.add_subplot(111)
-ax.boxplot(full_set)
-plt.xticks([1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17], ['2-Cluster AE', '3-Cluster AE', '4-Cluster AE','5-Cluster AE','6-Cluster AE','7-Cluster AE','8-Cluster AE','9-Cluster AE','10-Cluster AE','11-Cluster AE','12-Cluster AE','13-Cluster AE','14-Cluster AE','15-Cluster AE','16-Cluster AE','17-Cluster AE','18-Cluster AE','19-Cluster AE'], rotation='vertical')
+ax.boxplot(full_set[1:])
+plt.xticks([1,2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18], ['2-Cluster', '3-Cluster ', '4-Cluster','5-Cluster','6-Cluster','7-Cluster','8-Cluster ','9-Cluster','10-Cluster','11-Cluster','12-Cluster','13-Cluster','14-Cluster','15-Cluster','16-Cluster','17-Cluster','18-Cluster','19-Cluster'], rotation='vertical')
 
-#ax.set_title('Number Microservices Generated Vs. Metho')
-ax.set_xlabel('Generation Method')
+ax.set_title('AutoEncoder Q-Value Analysis ~ 750 Repository Dataset')
+ax.set_xlabel('N_Cluster Size')
 ax.set_ylabel('Q-Value')
 #plt.ylim(0, 20) 
 
---
+++ lv_q_value.py
@@ -0,0 +1,52 @@
+import os
+import  matplotlib.pyplot as plt
+
+source_dir = "experiment/test/WeatherIconView"
+#source_dir = "experiment/test"
+
+#Analyis meaning number of microservices generated
+louvain = []
+autoencoder = []
+
+
+data = [louvain, autoencoder]
+
+
+
+for r, d, f in os.walk(source_dir):
+    qvalue_file = r+"/"+r.split("/")[-1] +".qvalues"
+
+    if  os.path.exists(qvalue_file):
+        highest_ae_qvalue =  -9999
+        all_lines = []
+        with open(qvalue_file) as fi:
+                all_lines = fi.read().splitlines()
+        for line in all_lines:
+            splt_line = line.split()
+
+            if splt_line[0] =="louvain":
+                louvain.append(float(splt_line[1]))
+            else:
+                if float(splt_line[1]) > highest_ae_qvalue:
+                    highest_ae_qvalue =  float(splt_line[1]) 
+
+        autoencoder.append(highest_ae_qvalue)
+        
+        
+
+
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
+ax.boxplot(data)
+plt.xticks([1,2], ['Louvain Clusters', 'AutoEncoder Clusters'])
+
+ax.set_title(' Louvain vs Combined AutoEncoder Q-Value Analysis ~ 750 Repository Dataset')
+ax.set_xlabel('Method')
+ax.set_ylabel('Q-Value')
+
+plt.show()
+
+
+
