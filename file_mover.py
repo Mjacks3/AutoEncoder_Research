444c3c8d8aa937dcbfccb11231dda1b1eace36c7
Malik <mjacks3@umbc.edu>
2020-01-20 17:02:48 -0500

    update

--- file_mover.py
+++ file_mover.py
@@ -13,20 +13,20 @@ large = 0
 for r, d, f in os.walk(source_dir):
 
     basename = r.split("/")[-1]
-    if basename+".txt" in f:
+    if basename+".txt" in f and len (f) >= 4:
 
         with open(source_dir+"/"+basename+"/"+basename+".txt") as f:
             temp = f.readlines()
             data_size.append(len(temp) )
             if len(temp) < 30 and small <= 250:
                 small += 1
-                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test/"+basename)  
+                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test2/"+basename)  
             elif 30 < len(temp) and len(temp) < 75 and med <= 250:
                 med += 1
-                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test/"+basename)  
+                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test2/"+basename)  
             elif 75 < len(temp)  and   large <= 250:
                 large+= 1
-                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test/"+basename)  
+                shutil.copytree("../../coba_test/coba/experiment/test/"+basename, "../../coba_test/coba/experiment/balanced_test2/"+basename)  
 
 print("files less than 30: " + str(small))
 print("files  30-75: " + str(med))
