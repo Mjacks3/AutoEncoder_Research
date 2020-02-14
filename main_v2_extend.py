867bd9b9c095e80645a8b4e9d307d0c2f34bf668
Malik <mjacks3@umbc.edu>
2020-02-13 23:51:58 -0500

    update

--- main_v2_extend.py
+++ main_v2_extend.py
@@ -206,6 +206,7 @@ def test(args):
     name = args.test_dataset.split("/")[-1]
     print(name)
 
+
     with open(args.test_dataset +"/"+name + "_" + str(args.num_clusters)+ "-"+ str(sub_epoch_count)+ ".clustering",'w+') as f:
         for node_id, cluster in zip(y, y_pred):
             f.write( str(node_id) + " " + str(cluster) +"\n")
@@ -216,135 +217,6 @@ def test(args):
     #node_clusters
     return 0
     
-
-
-
-
-def calculate_metrics(clusters, edge_list='' ):
-    #cluster_pairs='/home/mjacks3/monize/tahdith/datasets/train/Java.git/Java.git.txt.clusters'
-    if not os.path.exists(edge_list):
-        print("Cannot Find Edge List. Exiting")
-        return 0
-
-    #Assemble Mappings for formula calculations
-    node_partition_mapping = {}
-    partion_node_counting = {}
-
-    for pair in clusters:
-        node_partion = pair.split()
-        node_partition_mapping[node_partion[0]] = node_partion[1]
-
-    for a in set(node_partition_mapping.values()):
-        partion_node_counting[a] = 0
-        for b in node_partition_mapping.items():
-            if str(b[1]) == str(a) :
-                partion_node_counting[a] += 1
-
-    #Calculate M_Norm, Sum of weights in graph
-    m_norm = 0
-
-    with open(edge_list) as f:
-        for edge in f:
-            m_norm += 1
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    #Modularity Q Calculation
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    modQ = 0 
-
-    for partition in partion_node_counting.keys():
-        internal = 0 
-        incident = 0
-        with open(edge_list) as f:
-            for edge in f:
-                source_dest = edge.split()
-
-                if node_partition_mapping[source_dest[0]] == str(partition) and \
-                node_partition_mapping[source_dest[1]] == str(partition):
-                    internal += 1
-                    incident += 1
-
-                elif node_partition_mapping[source_dest[0]] == str(partition) or \
-                node_partition_mapping[source_dest[1]] == str(partition):
-
-                    incident += 1
-
-        modQ += ( (internal/(2.0*m_norm)) -   (incident/(2.0*m_norm))**2)
-
-    print("Modularity Q Value")
-    print(modQ)
-    """
-
-
-
-    for partition_a in partion_node_counting.keys():
-        for partition_b in partion_node_counting.keys():
-            if partition_a == partition_b:
-
-                
-                actual_edges =  0 
-                possible_edges =   (partion_node_counting[partition_a] - 1) * partion_node_counting[partition_b]
-
-                with open(edge_list) as f:
-                        for edge in f:
-                            source_dest = edge.split()
-
-                            if source_dest[0] not in node_partition_mapping:
-                                #print("unmapped node: " , source_dest[0])
-                                pass #A bug if hit;  each node should be mapped
-
-                            elif source_dest[1] not in node_partition_mapping:
-                                #print("unmapped node: " , source_dest[1])
-                                pass #A bug if hit;  each node should be mapped
-
-
-                            elif (str(node_partition_mapping[source_dest[0]]) == partition_a and \
-                                str(node_partition_mapping[source_dest[1]]) == partition_b):
-
-                                actual_edges += 1
-                #print(partition_a + " to " + partition_b)
-                #print(str(actual_edges)+ "/" + str(possible_edges))
-                #print()
-
-
-
-                
-
-            else:
-                #print(partition_a+" "  partition_b " ", end="")
-                actual_edges =  0 
-                possible_edges =  2* partion_node_counting[partition_a] * partion_node_counting[partition_b]
-
-                with open(edge_list) as f:
-                        for edge in f:
-                            source_dest = edge.split()
-
-                            if source_dest[0] not in node_partition_mapping:
-                                #print("unmapped node: " , source_dest[0])
-                                pass #A bug if hit;  each node should be mapped
-
-                            elif source_dest[1] not in node_partition_mapping:
-                                #print("unmapped node: " , source_dest[1])
-                                pass #A bug if hit;  each node should be mapped
-
-
-                            elif (str(node_partition_mapping[source_dest[0]]) == partition_a and \
-                                str(node_partition_mapping[source_dest[1]]) == partition_b)  or \
-                                (str(node_partition_mapping[source_dest[0]]) == partition_b and \
-                                str(node_partition_mapping[source_dest[1]]) == partition_a):
-
-                                actual_edges += 1
-                #print(partition_a + " to " + partition_b)
-                #print(str(actual_edges)+ "/" + str(possible_edges))
-                #print()
-    """
-                
-    #print(partion_node_counting)  
-
-    #plt.plot([10], [modQ], 'ro')
-    #plt.show()
-
 
 
 if __name__ == "__main__":
@@ -428,7 +300,7 @@ if args.experiment:
     """
     
     
-    
+    """
     #pretrained_weights
     #Training
     models = [10,19,2,18,4,17,11,3,6,14]
@@ -446,23 +318,31 @@ if args.experiment:
         train(args)
     
     #End Training
+    """
     
     
     
  
-    """
     
-    models = [10]
+    
+    models = [10,19,2,18,4,17,11,3,6,14]
     for num_clusters in models:
-        for sub_epoch_count in range(10,500,10):
-            print(sub_epoch_count)
-            for r, d, f in os.walk("experiment/case_study/Acmeair"): 
-                if len(f) >= 4 and r.split("/")[-1] +  "_" + str(args.num_clusters)+ "-"+ str(sub_epoch_count)+ ".clustering" not in f  :
-                    args.num_clusters = num_clusters
-                    args.weights = "experiment/round_3/"+str(num_clusters)+"/"+str(sub_epoch_count)+"/model_final.h5"
-                    args.test_dataset = r
-                    test(args)
-    """
+        print(num_clusters)
+        args.num_clusters = num_clusters
+
+        for sub_epoch_count in range(100,500,100):
+
+            for r, d, f in os.walk("experiment/case_study"): 
+
+                if len(f) > 1 and r.split("/")[-1] +  "_" + str(args.num_clusters)+ "-"+ str(sub_epoch_count)+ ".clustering" not in f  :
+                    if os.path.exists("experiment/round_3/"+str(num_clusters)+"/"+str(sub_epoch_count)+"/model_final.h5"):
+                        
+                        args.weights = "experiment/round_3/"+str(num_clusters)+"/"+str(sub_epoch_count)+"/model_final.h5"
+                        args.test_dataset = r
+                        test(args)
+                else:
+                    print(r.split("/")[-1] +  "_" + str(args.num_clusters)+ "-"+ str(sub_epoch_count)+ ".clustering")
+        
 
 
 
